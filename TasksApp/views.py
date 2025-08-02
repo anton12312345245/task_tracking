26b3c89de064432bf9566efa72510eaad4c4fdc4
unknown <antonsibra836@gmail.com>
2025-08-02 18:44:16 +0200

    02 08 2025

--- TasksApp/views.py
+++ TasksApp/views.py
@@ -1,21 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
-from TasksApp.models import Task
+from TasksApp.models import Task,Comment
 from django.urls import reverse_lazy
 from TasksApp.forms import TaskForm
 from django.contrib.auth.mixins import  LoginRequiredMixin
 from TasksApp.mixins import UserIsOwnerMixin
+from TasksApp.forms import TaskFilterForm,CommentForm
 
 class TaskListViews(ListView):
     model = Task
     template_name = 'TaskList.html'
     context_object_name = 'tasks'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = TaskFilterForm(self.request.GET)
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        status = self.request.GET.get('status','')
+        priority = self.request.GET.get('priority','')
+        if status:
+            queryset = queryset.filter(status=status)
+        if priority:
+            queryset = queryset.filter(priority=priority)
+        return queryset
 
+    
 class TaskDetailViews(DetailView):
     model = Task
     template_name = 'TaskDetail.html'
     context_object_name = 'task_info'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        return context 
+    
+    def post(self,request,*args,**kwargs):
+        Comment_Form = CommentForm(request.POST)
+        if Comment_Form.is_valid():
+            comment = Comment_Form.save(commit=False)
+            comment.creator =  request.user
+            comment.task = self.get_object()
+            comment.save()
+            return redirect('TaskDetail',pk=comment.task.pk)
 
 
 class TaskCreateView(CreateView):
@@ -47,3 +76,10 @@ class DeleteTaskView(LoginRequiredMixin,UserIsOwnerMixin,DeleteView):
     success_url = reverse_lazy('TaskList')
 
 
+class DeleteComment(LoginRequiredMixin,UserIsOwnerMixin,DeleteView):
+    model = Comment
+    template_name = 'CommentDelete.html'
+    def get_success_url(self):
+        return reverse_lazy('TaskDetail',kwargs={'pk':self.object.task.pk}) 
+
+    
